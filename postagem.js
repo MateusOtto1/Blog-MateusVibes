ce87f62f413c1189a84d62613560619670b32028
Mateus <mateus-botto@educar.rs.gov.br>
2023-08-14 14:41:09 -0300

    Implementação de botão ver mais

--- postagem.js
+++ postagem.js
@@ -2,14 +2,16 @@ let btn1 = document.querySelector(".btn1");
 let btn2 = document.querySelector(".btn2");
 let btn3 = document.querySelector(".btn3");
 let btn4 = document.querySelector(".btn4");
-
-btn1 = 1;
-btn2 = 2;
-btn3 = 3;
-btn4 = 4;
+let container = document.querySelector(".container-mais");
 
 btn1.addEventListener('click', ()=>{
+    container.innerHTML = "";
     fetch(`https://api-rest-post-diegocandido.herokuapp.com/postagem/1`).then(result => result.json()).then(result =>{
-
+        card.innerHTML = result.title;
+        document.querySelector(".descricao-post2").innerHTML = result.description;
+        document.querySelector(".descricao-post2").style.display = "none";
+        document.querySelector(".nome-criador2").innerHTML = result.profileName;
+        let imgpost2 = `https://api-rest-post-diegocandido.herokuapp.com${result.thumbImage}`;
+        document.querySelector(".img-post2").src = imgpost2;
     });
 })
